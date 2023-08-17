a3d6f7442ff00ca8a138f1018b47c417c9f712d8
Ignat Ignatov <ignatov_rs@abv.bg>
2023-08-17 10:26:37 +0300

    add Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import {} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Details from "./components/Details/Details";
+import Create from "./components/Create/Create";
+import Edit from "./components/Edit/Edit";
+import MyPets from "./components/MyPets/MyPets";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div>
-     
+    <div id="container">
+      <Header />
+
+      <main id="site-content">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/my-pets" element={<MyPets />} />
+
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
